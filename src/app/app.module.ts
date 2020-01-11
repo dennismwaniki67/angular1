fe35a97bfbd4420988452d8114207a77a233096e
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-01-11 07:26:42 +0300

    used the selector for the GoalDetailComponent and nested it into the GoalComponent, placing it after the goal name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    GoalDetailComponent
+    GoalDetailComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule
