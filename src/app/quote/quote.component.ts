f058d0668e6dd28d2448cb63516fdd7f4c00b341
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-01-11 13:23:39 +0300

    used boostrap classes in our HTML templates tags and the bootstrap grid layout also in goal-detail.component.html and goal.component.html

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1,'Watch finding Nemo','Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+    new Quote(1,'You only live once, but if you do it right, once is enough.','Author:Mae West',new Date(2020,3,14)),
+    new Quote(2,'Life is what happens to us while we are making other plans.','Author:Allen Saunders',new Date(2020,3,14)),
+    new Quote(3,'Everything you can imagine is real.','Author:Daniel Ndambuki',new Date(2020,3,14)),
+    new Quote(4,'Sometimes the questions are complicated and the answers are simple.','Author:Pablo Picasso',new Date(2020,3,14)),
+    new Quote(5,'Solve math homework','Author:Damn Math',new Date(2020,3,14)),
+    new Quote(6,'Plot my world domination plan','Author:Daniel Ndambuki',new Date(2020,3,14)),
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
