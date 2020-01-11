d1066bb0a77ed8f35702fc2e2cfd9f0925a34eb8
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-01-11 07:13:19 +0300

    grnerated a goal-detail receiving the goal description to display from the parent component, GoalComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    GoalDetailComponent
   ],
   imports: [
     BrowserModule
