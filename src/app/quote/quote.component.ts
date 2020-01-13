4a92407ae5217cb4d788c335902be4f9e0c29176
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-01-13 13:10:09 +0300

    added more css in our project

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1,'You only live once, but if you do it right, once is enough.','Author:Mae West',new Date(2020,3,14)),
-    new Quote(2,'Life is what happens to us while we are making other plans.','Author:Allen Saunders',new Date(2020,3,14)),
-    new Quote(3,'Everything you can imagine is real.','Author:Daniel Ndambuki',new Date(2020,3,14)),
-    new Quote(4,'Sometimes the questions are complicated and the answers are simple.','Author:Pablo Picasso',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Author:Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Author:Daniel Ndambuki',new Date(2020,3,14)),
+    new Quote(1,'You only live once, but if you do it right, once is enough.','Mae West',new Date(2020,3,14)),
+    new Quote(2,'Life is what happens to us while we are making other plans.','Allen Saunders',new Date(2020,3,14)),
+    new Quote(3,'Everything you can imagine is real.','Daniel Ndambuki',new Date(2020,3,14)),
+    new Quote(4,'Sometimes the questions are complicated and the answers are simple.','Pablo Picasso',new Date(2020,3,14)),
+    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
+    new Quote(6,'Plot my world domination plan','Daniel Ndambuki',new Date(2020,3,14)),
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
@@ -24,7 +24,7 @@ export class QuoteComponent implements OnInit {
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
-  deleteGoal(isComplete, index){
+  deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
